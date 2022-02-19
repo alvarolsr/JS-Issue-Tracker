14fad7e7546ad9e841aa685a70d57dc437d65d38
Alvaro Salazar <alvarolsr@gmail.com>
2022-02-18 23:31:31 -0400

    Fixed error in index.html
    Changed "issueInputForm" class to id again, so it can be gotten from the DOM by its ID.

--- js/script.js
+++ js/script.js
@@ -1,70 +1,101 @@
-function saveIssue() {
-  var issueDesc = document.getElementById("issueDescInput").value;
-  var issueId = chance.guid();
-
-  var issue = {
-    id: issueId,
-    description: issueDesc,
-  };
-
-  var issuesList = document.getElementById("issuesList");
-
-  issuesList.innerHTML +=
-    '<div class="issue">' +
-    "<h6>Issue ID: " +
-    issue[id] +
-    "perro" +
-    "</h6>" +
-    "<h6>Issue desc: " +
-    issue[desc] +
-    "</h6></div>";
-}
-
 function addIssue() {
-  var issueDesc = document.getElementById("issueDescInput").value;
-  var issueId = chance.guid();
+  let issueDesc = document.getElementById("issueDescInput").value;
+  let issueSeverity = document.getElementById("issueSeverityInput").value;
+  let issueAssignedTo = document.getElementById("issueAssignedToInput").value;
+  let issueId = chance.guid();
+  let issueStatus = "Open";
 
-  var issue = {
+  //var issuesList = document.getElementById("issuesList").innerHTML;
+
+  let issue = {
     id: issueId,
     description: issueDesc,
+    severity: issueSeverity,
+    responsible: issueAssignedTo,
+    status: issueStatus,
   };
 
   if (localStorage.getItem("issues") == null) {
-    var issues = [];
+    let issues = [];
     issues.push(issue);
     localStorage.setItem("issues", JSON.stringify(issues));
   } else {
-    var issues = JSON.parse(localStorage.getItem("issues"));
+    let issues = JSON.parse(localStorage.getItem("issues"));
     issues.push(issue);
     localStorage.setItem("issues", JSON.stringify(issues));
   }
-  // added
-  console.log(issues);
 
   document.getElementById("issueInputForm").reset();
 
-  /*showIssues();*/
+  console.log("done");
 
-  e.preventDefault();
-  /*issuesList.innerHTML +=
-    '<div class="issue">' + "<h6>Issue desc: " + issue + "</h6>" + "</div>";*/
+  showIssues();
+}
+
+function deleteIssue(id) {
+  let issues = JSON.parse(localStorage.getItem("issues"));
+
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+  localStorage.setItem("issues", JSON.stringify(issues));
+
+  showIssues();
+}
+
+function setStatusClosed(id) {
+  let issues = JSON.parse(localStorage.getItem("issues"));
+
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = "Closed";
+    }
+  }
+
+  localStorage.setItem("issues", JSON.stringify(issues));
+
+  showIssues();
 }
 
 function showIssues() {
-  var issues = JSON.parse(localStorage.getItem("issues"));
-  var issuesList = document.getElementById("issuesList");
-
-  console.log(issues);
+  let issues = JSON.parse(localStorage.getItem("issues"));
+  let issuesList = document.getElementById("issuesList");
 
   issuesList.innerHTML = "";
 
-  for (var i = 0; i < issues.length; i++) {
-    var id = issues[i].id;
-    var desc = issues[i].description;
+  for (let i = 0; i < issues.length; i++) {
+    let issue = issues[i];
+    let issueId = issue.id;
+    let issueDesc = issue.description;
+    let issueSeverity = issue.severity;
+    let issueResponsible = issue.responsible;
+    let issueStatus = issue.status;
 
     issuesList.innerHTML +=
       '<div class="issue">' +
-      "<h6>Issue ID: ${id} </h6>" +
-      "<h6>Issue desc: ${desc} </h6></div>";
+      "<h6>Issue ID: " +
+      issueId +
+      "</h6>" +
+      "<h6>Issue desc: " +
+      issueDesc +
+      "</h6>" +
+      "<h6>Issue Severity: " +
+      issueSeverity +
+      "</h6>" +
+      "<h6>Assigned To: " +
+      issueResponsible +
+      "</h6>" +
+      "<h6>Issue Status: " +
+      issueStatus +
+      "</h6>" +
+      "<button type='button' onclick='setStatusClosed(\"" +
+      issueId +
+      "\")'>Close</button>" +
+      "<button type='button' onclick='deleteIssue(\"" +
+      issueId +
+      "\")'>Delete</button>";
+    ("</div>");
   }
 }
